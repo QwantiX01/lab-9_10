4bf81a2d3099f9837e63244dee71a51300c5eaef
Yurii Bizhyk <qwantiix01@gmail.com>
2026-04-28 22:25:16 +0300

    add routing, entry point and global styles

--- railway/src/App.jsx
+++ railway/src/App.jsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
